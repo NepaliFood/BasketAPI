f3f92cdd0851abdd832dc3b7ef8ecd2915654c5d
Naresh Kumar Rai <n.rai@student.fontys.nl>
2022-04-21 20:24:00 +0200

    Creates Entities

---
+++ BasketAPI/Entities/ShoppingCart.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasketAPI.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalprice = 0;
+                foreach (var item in Items)
+                {
+                    totalprice += item.Price * item.Quantity;
+                }
+                return totalprice;
+            }
+        }
+
+        public ShoppingCart()
+        {
+
+        }
+        public ShoppingCart(string username)
+        {
+            this.UserName = username;
+        }
+
+        
+    }
+}
